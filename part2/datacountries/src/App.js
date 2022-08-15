b30960364cdde3d6b5273ea182187e9e3e804c91
Diego Ramos <diegorramos84@gmail.com>
2022-08-15 10:43:37 +0100

    cleaning up

--- part2/datacountries/src/App.js
+++ part2/datacountries/src/App.js
@@ -3,6 +3,8 @@ import axios from "axios";
 import Filter from './components/Filter'
 import CountriesList from './components/CountriesList'
 
+// 924f940fe764a64f6f88233a84bba9d0
+
 function App() {
 
   const [countries, setCountries] = useState([])
@@ -22,7 +24,10 @@ function App() {
     setFilterCountry(event.target.value)
   }
 
+  // Set a function to handle when we click the show button in the country
   const handleShowCountry = (countriesNames) => {
+    // this function takes the country we clicked and set the filter to the name
+    // of that country
     setFilterCountry(countriesNames.name.common)
   }
 
@@ -30,6 +35,7 @@ function App() {
     <div>
       <Filter filterCountry={filterCountry} handleFilterChange={handleFilterChange} />
       <CountriesList
+      // we pass the function to handle the click to the component
       handleShowCountry = {handleShowCountry}
       countries={countries}
       filterCountry={filterCountry} />
