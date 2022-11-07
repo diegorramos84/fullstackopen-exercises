c489541e1df252cecc92d5f41caaaedeaeda0418
Diego Ramos <diegorramos84@gmail.com>
2022-11-07 16:26:30 +0000

    added service login

--- part2/notes/src/App.js
+++ part2/notes/src/App.js
@@ -3,6 +3,7 @@ import { useState, useEffect } from 'react'
 import Note from './components/Note'
 import Notification from './components/Notification'
 import noteService  from './services/notes'
+import loginService from './services/login'
 
 const Footer = () => {
   const footerStyle = {
@@ -24,6 +25,9 @@ const App = () => {
   const [newNote, setNewNote] = useState('')
   const [showAll, setShowAll] = useState(true)
   const [errorMessage, setErrorMessage] = useState(null)
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [user, setUser] = useState(null)
 
   useEffect(() => {
     noteService
@@ -32,6 +36,24 @@ const App = () => {
         setNotes(initialNotes)
       })
   }, [])
+
+  const handleLogin = async (event) => {
+    event.preventDefault()
+    try {
+      const user = await loginService.login({
+        username, password,
+      })
+      noteService.setToken(user.token)
+      setUser(user)
+      setUsername('')
+      setPassword('')
+    } catch (exception) {
+      setErrorMessage('Wrong credentials')
+      setTimeout(() => {
+        setErrorMessage(null)
+      }, 5000)
+    }
+  }
 
   const addNote = (event) => {
     event.preventDefault()
@@ -69,25 +91,70 @@ const App = () => {
       })
   }
 
-
   const handleNoteChange = (event) => {
     console.log(event.target.value)
     setNewNote(event.target.value)
   }
 
+
+
   const notesToShow = showAll
     ? notes
     : notes.filter(note => note.important)
+
+  const loginForm = () => (
+    <form onSubmit={handleLogin}>
+      <div>
+        username
+          <input
+          type="text"
+          value={username}
+          name="Username"
+          onChange={({ target }) => setUsername(target.value)}
+          />
+      </div>
+      <div>
+        password
+          <input
+          type="text"
+          value={password}
+          name="Password"
+          onChange={({ target }) => setPassword(target.value)}
+          />
+      </div>
+    <button type="submit">login</button>
+    </form>
+  )
+
+  const noteForm = () => (
+    <form onSubmit={addNote}>
+      <input
+        value={newNote}
+        onChange={handleNoteChange}
+      />
+      <button type="submit">save</button>
+    </form>
+  )
 
   return (
     <div>
       <h1>Notes</h1>
       <Notification message={errorMessage} />
+
+      {user === null ?
+        loginForm() :
+        <div>
+          <p>{user.name} logged in</p>
+          {noteForm()}
+        </div>
+      }
+
       <div>
         <button onClick={() => setShowAll(!showAll)}>
           show {showAll ? 'important' : 'all' }
         </button>
       </div>
+
       <ul>
         {notesToShow.map(note =>
           <Note
@@ -97,13 +164,7 @@ const App = () => {
           />
         )}
       </ul>
-      <form onSubmit={addNote}>
-        <input
-          value={newNote}
-          onChange={handleNoteChange}
-        />
-        <button type="submit">save</button>
-      </form>
+
       <Footer />
     </div>
   )
--- part2/notes/src/services/notes.js
+++ part2/notes/src/services/notes.js
@@ -1,13 +1,22 @@
 import axios from 'axios'
 const baseUrl = '/api/notes'
 
+let token = null
+
+const setToken = newToken => {
+  token = `bearer ${newToken}`
+}
+
 const getAll = () => {
   const request =  axios.get(baseUrl)
   return request.then(response => response.data)
 }
 
-const create = newObject => {
-  const request =  axios.post(baseUrl, newObject)
+const create = async newObject => {
+  const config = {
+    headers: { Authorization: token }
+  }
+  const request =  axios.post(baseUrl, newObject, config)
   return request.then(response => response.data)
 }
 
@@ -16,4 +25,4 @@ const update = (id, newObject) => {
   return request.then(response => response.data)
 }
 
-export default { getAll, create, update }
+export default { getAll, create, update, setToken }
